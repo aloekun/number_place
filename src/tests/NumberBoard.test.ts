94c4e13a792ea8fd3348af00c0bbaf835f2151a9
aloekun <56211144+aloekun@users.noreply.github.com>
2024-12-19 02:27:10 +0900

    getNumberSetの戻り値が予想できなかったので、配列のatに近い挙動に修正

--- src/tests/NumberBoard.test.ts
+++ src/tests/NumberBoard.test.ts
@@ -3,8 +3,9 @@ import { NumberBoard } from "../logics/NumberBoard";
 test("横9つの1列目を一括取得", () => {
     const sut = new NumberBoard([1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
-    const numberSet: number[] = sut.getNumberSet(1);
+    const resultSet = sut.getNumberSet(1);
 
+    const numberSet = resultSet !== undefined ? resultSet : [];
     const numberSetStr: string = numberSet.toString();
     const targetStr: string = [1, 1, 1, 1, 1, 1, 1, 1, 1].toString();
     expect(numberSetStr).toBe(targetStr);
@@ -13,8 +14,9 @@ test("横9つの1列目を一括取得", () => {
 test("横9つの1列目を一括取得(バラけた数字)", () => {
     const sut = new NumberBoard([1, 2, 3, 1, 1, 1, 1, 1, 1]);
 
-    const numberSet: number[] = sut.getNumberSet(1);
+    const resultSet = sut.getNumberSet(1);
 
+    const numberSet = resultSet !== undefined ? resultSet : [];
     const numberSetStr: string = numberSet.toString();
     const targetStr: string = [1, 2, 3, 1, 1, 1, 1, 1, 1].toString();
     expect(numberSetStr).toBe(targetStr);
@@ -25,8 +27,9 @@ test("横9つの2列目を一括取得", () => {
         1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2,
     ]);
 
-    const numberSet: number[] = sut.getNumberSet(2);
+    const resultSet = sut.getNumberSet(2);
 
+    const numberSet = resultSet !== undefined ? resultSet : [];
     const numberSetStr: string = numberSet.toString();
     const targetStr: string = [2, 2, 2, 2, 2, 2, 2, 2, 2].toString();
     expect(numberSetStr).toBe(targetStr);
@@ -35,9 +38,9 @@ test("横9つの2列目を一括取得", () => {
 test("横9つの範囲外を一括取得(0の配列を返す)", () => {
     const sut = new NumberBoard([1, 1, 1, 1, 1, 1, 1, 1, 1]);
 
-    const numberSet: number[] = sut.getNumberSet(10);
+    const resultSet = sut.getNumberSet(10);
 
-    const numberSetStr: string = numberSet.toString();
-    const targetStr: string = [0, 0, 0, 0, 0, 0, 0, 0, 0].toString();
+    const numberSetStr: string = resultSet === undefined ? "undefined" : "";
+    const targetStr: string = "undefined";
     expect(numberSetStr).toBe(targetStr);
 });
